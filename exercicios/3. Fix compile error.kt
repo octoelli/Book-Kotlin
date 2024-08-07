f1e3636600e8da6290c137dc40a321b4e2755d32
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-06 21:08:48 -0300

    Update 3. Fix compile error.kt
    Signed-off-by: Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>

--- exercicios/3. Fix compile error.kt
+++ exercicios/3. Fix compile error.kt
@@ -1,12 +1,12 @@
 /*
 3. Corrigir erro de compilação
 Este programa imprime uma mensagem que notifica o usuário de que ele recebeu uma mensagem de bate-papo de um amigo.
-*/
+
 fun main() { 
     println("New chat message from a friend'}
 }
 
-/*
+
 Você consegue descobrir a causa raiz dos erros de compilação neste programa e corrigi-los?
 O código usa símbolos apropriados para indicar a abertura e o fechamento da string e do argumento da função?
 Dica: você pode usar o Kotlin Playground para executar o código e visualizar os erros de compilação.
@@ -22,7 +22,7 @@ O código continha dois erros de compilação:
 
 A sequência deve terminar com aspas duplas em vez de aspas simples.
 O argumento da função deve terminar com um parêntesis em vez de uma chave.
-/*
+*/
 fun main() { 
     println("New chat message from a friend")
 }
