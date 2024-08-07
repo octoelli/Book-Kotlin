f0e9237ac2d7838172cb25f1e3c025594b95f42b
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-07 15:04:30 -0300

    Create Hello_world.kt
    Criando a primeira introdução com os exemplos basicos
    
    Signed-off-by: Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>

---
+++ introducao/Hello_world.kt
@@ -0,0 +1,13 @@
+/**
+ * Olá, Mundo!
+ *
+ * @see [Hello World](https://play.kotlinlang.org/byExample/01_introduction/01_Hello%20world)
+ */
+
+package eu.coelli.helloworld           // 1. Definição do pacote, algo muito comum para organização de código (apesar de opcional).
+
+fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
+    println("Olá, Mundo!")          // 3. Imprime na saída padrão um texto usando a função [println] (importada implicitamente).
+}
+
+Em um navegador da Web no computador, abra o Playground Kotlin: https://play.kotlinlang.org/
