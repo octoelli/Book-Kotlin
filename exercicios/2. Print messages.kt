841a28a155e4282980b6705c9f673357693f7c15
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-06 21:02:49 -0300

    Create 2. Print messages.kt
    Signed-off-by: Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>

---
+++ exercicios/2. Print messages.kt
@@ -0,0 +1,18 @@
+/*
+Você pode escrever uma main()função que imprima essas mensagens em quatro linhas separadas?
+
+Use the val keyword when the value doesn't change. 
+Use the var keyword when the value can change.
+When you define a function, you define the parameters that can be passed to it. 
+When you call a function, you pass arguments for the parameters.
+
+
+A solução usa a println()função para imprimir as mensagens em cada linha.
+*/
+
+fun main() {
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it.")
+    println("When you call a function, you pass arguments for the parameters.")
+}
