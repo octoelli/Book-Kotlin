d0910627d9bc0e7c9edde64d3102166b69d0bbd6
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-07 15:16:53 -0300

    Update Hello_world.kt
    Signed-off-by: Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>

--- introducao/Hello_world.kt
+++ introducao/Hello_world.kt
@@ -10,4 +10,15 @@ fun main() {                        // 2. Ponto de entrada de um código Kotlin,
     println("Olá, Mundo!")          // 3. Imprime na saída padrão um texto usando a função [println] (importada implicitamente).
 }
 
+/*
 Em um navegador da Web no computador, abra o Playground Kotlin: https://play.kotlinlang.org/
+Quando você clica no botão Executar, muita coisa acontece. O código na linguagem de programação Kotlin deve ser entendido por humanos,
+para que as pessoas possam ler, escrever e colaborar com mais facilidade em programas Kotlin. No entanto, seu computador não entende
+imediatamente esse idioma.
+
+Você precisa de algo chamado compilador Kotlin, que pega o código Kotlin que você escreveu, analisa-o linha por linha e o traduz em algo 
+que o computador possa entender. Esse processo é chamado de compilação do código.
+
+Se o código for compilado com êxito, o programa será executado (ou executado). Quando o computador executa seu programa, ele executa cada
+uma de suas instruções. Se você já seguiu uma receita culinária, executar cada etapa da receita é considerado executar cada instrução.
+*/
