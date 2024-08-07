869bc9867789c2a2bca4dd00d7dd096cf26b1e3a
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-07 15:17:48 -0300

    Update Hello_world.kt
    Atualização
    
    Signed-off-by: Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>

--- introducao/Hello_world.kt
+++ introducao/Hello_world.kt
@@ -4,7 +4,7 @@
  * @see [Hello World](https://play.kotlinlang.org/byExample/01_introduction/01_Hello%20world)
  */
 
-package eu.coelli.helloworld           // 1. Definição do pacote, algo muito comum para organização de código (apesar de opcional).
+package eu.octoelli.helloworld           // 1. Definição do pacote, algo muito comum para organização de código (apesar de opcional).
 
 fun main() {                        // 2. Ponto de entrada de um código Kotlin, definido pela função [main].
     println("Olá, Mundo!")          // 3. Imprime na saída padrão um texto usando a função [println] (importada implicitamente).
